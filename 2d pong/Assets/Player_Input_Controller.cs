e41f839e12afe40d7dfe257bd6703cf8414d742e
Phillip <phillipables1@gmail.com>
2018-01-27 08:04:49 -0500

    if movement reader statements with debuggers to check key input for guest

--- 2d pong/Assets/Player_Input_Controller.cs
+++ 2d pong/Assets/Player_Input_Controller.cs
@@ -15,11 +15,35 @@ public class Player_Input_Controller : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //if guest is pressing the w key
+        if (Input.GetKey(KeyCode.W))
+        {
+            //move the bat up
+            
+            Debug.Log("Guest is pressing W");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //move the bat down
+
+            Debug.Log("Guest is pressing S");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            //move the bat left
+
+            Debug.Log("Guest is pressing A");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            //move the bat right
+
+            Debug.Log("Guest is pressing D");
+        }
+        //if guest is pressing the s key
+    }
 }
