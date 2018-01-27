5b0e2225e19243f12a934f59684cc3ecf82e5d93
Phillip <phillipables1@gmail.com>
2018-01-27 07:07:32 -0500

    2d view with player, guest, ball, light source, and player input object

---
+++ 2d pong/Assets/Player_Input_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Input_Controller : MonoBehaviour {
+
+    //script that handles input from two players
+    //player 1 => controls guest with W/S ksys
+    //player 2 => controls player with arrow keys
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
