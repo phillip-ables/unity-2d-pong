6d3c200603ba38a06fcf49478f095fcb2f558a0a
Phillip <phillipables1@gmail.com>
2018-01-27 08:25:05 -0500

    velocity vector change if key press

--- 2d pong/Assets/Player_Input_Controller.cs
+++ 2d pong/Assets/Player_Input_Controller.cs
@@ -20,25 +20,25 @@ public class Player_Input_Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         //if guest is pressing the w key
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             //move the bat up
-            
-        }
-        else if (Input.GetKey(KeyCode.S))
+            guest.GetComponent<Rigidbody>().velocity = new Vector3(0f, 1f, 0f);
+         }
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             //move the bat down
-
+            guest.GetComponent<Rigidbody>().velocity = new Vector3(0f, -1f, 0f);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             //move the bat left
-
+            guest.GetComponent<Rigidbody>().velocity = new Vector3(-1f, 0f, 0f);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.D))
         {
             //move the bat right
-
+            guest.GetComponent<Rigidbody>().velocity = new Vector3(1f, 0f, 0f);
         }
         //if guest is pressing the s key
     }
