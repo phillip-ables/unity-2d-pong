6e2d50a9436003f18324315b528874f6cb1db5dc
Phillip <phillipables1@gmail.com>
2018-01-27 21:28:57 -0500

    size of the arena changes at random intervals, error-> bounds do not

--- 2d pong/Assets/Player_Input_Controller.cs
+++ 2d pong/Assets/Player_Input_Controller.cs
@@ -11,11 +11,28 @@ public class Player_Input_Controller : MonoBehaviour {
     public GameObject player;
     public GameObject guest;
     public int CharVel;
+    public int screenJumpCount;
+    public object halfHeight;
+    public object halfWidth;
+    public void screenJump()
+    {
+        Camera.main.orthographicSize = Random.Range(2, 8);
+        Debug.Log("New Camera Orthographics: "+Camera.main.orthographicSize);
+
+        iterateSet();
+        screenJumpCount = 0;
+        halfHeight = Screen.height / 34.1 / 2;
+        halfWidth = Screen.width / 34.1 / 2;
+    }
+    public void iterateSet()
+    {
+        var randomIterate = Random.Range(10, 1000000000000);
+    }
 
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Camera Orthographics: "+Camera.main.orthographicSize);
     }
 
     // Update is called once per frame
@@ -27,67 +44,82 @@ public class Player_Input_Controller : MonoBehaviour {
         var guestVert = guest.transform.position.y;
         var guestHor = guest.transform.position.x;
         var halfHeight = Screen.height/34.1/ 2;
-        var halfWidth = Screen.width/34.1/ 2;
+        var halfWidth = Screen.width / 34.1 / 2;
+        var randomIterate = 10;
+
+        Debug.Log("Random Ieterate: "+randomIterate);
+
+        /*
+         * counter which increments everytime a button is keyed
+         * when randomIterate is equal to screenJumpCount
+         * call screenSizeJump to randomly set the width and height
+         * sets new increment value
+         */
+         if(screenJumpCount >= randomIterate)
+        {
+            screenJump();
+        }
 
         if (Input.GetKey(KeyCode.UpArrow)
-            && playerVert < halfHeight)
+            && playerVert < halfHeight)//move the bat up
         {
-            //move the bat up
             player.GetComponent<Rigidbody>().velocity = new Vector3(0f, CharVel, 0f);
+            screenJumpCount++;
         }
         else if (Input.GetKey(KeyCode.DownArrow)
-                 && playerVert > -halfHeight)
+                 && playerVert > -halfHeight)//move the bat down
         {
-            //move the bat down
             player.GetComponent<Rigidbody>().velocity = new Vector3(0f, -CharVel, 0f);
+            screenJumpCount++;
         }
         else if (Input.GetKey(KeyCode.LeftArrow) 
-                 && playerHor > 0 )
+                 && playerHor > 0)//move the bat left
         {
-            //move the bat left
             player.GetComponent<Rigidbody>().velocity = new Vector3(-CharVel, 0f, 0f);
-
+            screenJumpCount++;
         }
         else if (Input.GetKey(KeyCode.RightArrow)
-                 && playerHor < halfWidth)
+                 && playerHor < halfWidth)//move the bat right
         {
-            //move the bat right
             player.GetComponent<Rigidbody>().velocity = new Vector3(CharVel, 0f, 0f);
+            screenJumpCount++;
         }
         else
         {
             player.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+            Debug.Log("Current ScreenJump Count: "+screenJumpCount);
         }
 
 
         //if guest is pressing the w key
         if (Input.GetKey(KeyCode.W)
-             && guestVert < halfHeight)
+             && guestVert < halfHeight)//move the bat up
         {
-            //move the bat up
             guest.GetComponent<Rigidbody>().velocity = new Vector3(0f, CharVel, 0f);
+            screenJumpCount++;
         }
         else if (Input.GetKey(KeyCode.S) 
-                 && guestVert > -halfHeight)
+                 && guestVert > -halfHeight)//move the bat down
         {
-            //move the bat down
             guest.GetComponent<Rigidbody>().velocity = new Vector3(0f, -CharVel, 0f);
+            screenJumpCount++;
         }
         else if (Input.GetKey(KeyCode.A) 
-                 && guestHor > -halfWidth)
+                 && guestHor > -halfWidth)//move the bat left
         {
-            //move the bat left
             guest.GetComponent<Rigidbody>().velocity = new Vector3(-CharVel, 0f, 0f);
+            screenJumpCount++;
         }
         else if (Input.GetKey(KeyCode.D) 
-             && guestHor < 0)
+             && guestHor < 0)// move the bat right
         {
-            //move the bat right
             guest.GetComponent<Rigidbody>().velocity = new Vector3(CharVel, 0f, 0f);
+            screenJumpCount++;
         }
         else
         {
             guest.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+            Debug.Log("Current Jump Count: " + screenJumpCount);
         }
 
     }
